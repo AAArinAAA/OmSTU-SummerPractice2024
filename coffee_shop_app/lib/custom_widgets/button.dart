ee001b28f6ab42472bc7bd92926a559c4c281b54
AAArinAAA <15days.before@gmail.com>
2024-07-04 00:43:19 +0600

    best button

--- coffee_shop_app/lib/custom_widgets/button.dart
+++ coffee_shop_app/lib/custom_widgets/button.dart
@@ -11,97 +11,102 @@ class ProductButton extends StatefulWidget {
 
 class _ProductButtonState extends State<ProductButton> {
   int totalItemsInCart = 0;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         setState(() {
-          if (totalItemsInCart < 10) {
-            totalItemsInCart++;
-          } else {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Максимальное количество товаров в корзине - 10'),
-              ),
-            );
-          }
+          // Toggle between displaying buttons and displaying the price
+          totalItemsInCart = totalItemsInCart == 0 ? 1 : 0;
         });
       },
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.ease,
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        decoration: BoxDecoration(
-          color: const Color.fromRGBO(205, 180, 219, 1),
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(
-              width: 100.0,
-              height: 25,
-              child: totalItemsInCart == 0
-                  ? Text(
-                      '       ${widget.price} ₽     ',
+      child: totalItemsInCart == 0
+          ? Container(
+              width: 138.0,
+              height: 35.0,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(205, 180, 219, 1) ,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: Center(
+                child: Text(
+                  '${widget.price} ₽',
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            )
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (totalItemsInCart > 0) {
+                        totalItemsInCart--;
+                      }
+                    });
+                  },
+                  child: Container(
+                    width: 35,
+                    height: 35,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromRGBO(205, 180, 219, 1) 
+                    ),
+                    child: const Icon(Icons.remove, color:  Color.fromRGBO(255, 255, 255, 1)),
+                  ),
+                ),
+                const SizedBox(width: 4,),
+                Container(
+                  width: 60,
+                  height: 35,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color:  const Color.fromRGBO(205, 180, 219, 1) ,
+                  ),
+                  child: Center(
+                    child: Text(
+                      '$totalItemsInCart',
                       style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
-                    )
-                  : Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              if (totalItemsInCart > 0) totalItemsInCart--;
-                            });
-                          },
-                          child: Container(
-                            width: 24.0,
-                            height: 24.0,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              color: Colors.white,
-                            ),
-                            child: const Icon(Icons.remove, color: Color.fromRGBO(205, 180, 219, 1)),
-                          ),
-                        ),
-                        const SizedBox(width: 8.0),
-                        Text('$totalItemsInCart', style: const TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold)),
-                        const SizedBox(width: 8.0),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              if (totalItemsInCart < 10) {
-                                totalItemsInCart++;
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text('Максимальное количество товаров в корзине - 10'),
-                                  ),
-                                );
-                              }
-                            });
-                          },
-                          child: Container(
-                            width: 24.0,
-                            height: 24.0,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
-                            ),
-                            child: const Icon(Icons.add, color: Color.fromRGBO(205, 180, 219, 1)),
-                          ),
-                        ),
-                      ],
                     ),
+                  ),
+                ),
+                const SizedBox(width: 4,),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (totalItemsInCart < 10) {
+                        totalItemsInCart++;
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Превышено количество товара в корзине'),
+                          ),
+                        );
+                      }
+                    });
+                  },
+                  child: Container(
+                    width: 35,
+                    height: 35,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color:  Color.fromRGBO(205, 180, 219, 1) ,
+                    ),
+                    child: const Icon(Icons.add, color: Color.fromRGBO(255, 255, 255, 1)),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
     );
   }
 }
